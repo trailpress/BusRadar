21edfaa025d166352b9e328014d8484466e8e389
trailpress <fabligo74@gmail.com>
2026-07-13 10:50:57 +0200

    Updates

---
+++ assets/SearchBox-BpYjgmf9.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CrGTioug.js";import{S as t}from"./search-Tq06d0tU.js";function i({value:r,placeholder:s,onChange:a}){return e.jsxs("label",{className:"search-box",children:[e.jsx(t,{size:16}),e.jsx("input",{value:r,placeholder:s,onChange:o=>a(o.target.value)})]})}export{i as S};
